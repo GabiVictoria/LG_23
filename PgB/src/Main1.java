d8a18da301f267d98db4d8a89d300c48798cf389
Victória <126773570+GabiVictoria@users.noreply.github.com>
2023-11-13 00:43:55 -0300

    Delete PgB directory

--- PgB/src/Main1.java
+++
@@ -1,9 +0,0 @@
-import text.SalvarEmArquivo;
-
-import java.io.IOException;
-
-public class Main1 {
-    public static void main(String[] args) throws IOException {
-        SalvarEmArquivo.lerDadosDoArquivo("investidores.txt");
-    }
-}
